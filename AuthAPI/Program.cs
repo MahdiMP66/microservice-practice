2eeeacc0ca5e72975e60c36054b32271b2b898a5
mahdimp <madanipour66@gmail.com>
2024-02-23 20:13:31 +0330

    added identity

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,3 +1,7 @@
+using AuthAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration
+    .GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
 
 var app = builder.Build();
 
